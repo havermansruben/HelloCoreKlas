af50fc0b0240f666bb63c442583c0ebbd07362aa
havermansruben <60925210+havermansruben@users.noreply.github.com>
2020-02-25 15:10:18 +0100

    Add files via upload

---
+++ Controllers/KlantController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HelloCore.Data;
+using HelloCore.Models;
+using HelloCore.ViewModels;
+
+namespace HelloCore.Controllers
+{
+    public class KlantController : Controller
+    {
+        private readonly HelloCoreContext _context;
+
+        public KlantController(HelloCoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Klant
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new ListKlantViewModel();
+            viewModel.Klanten = await _context.Klanten.ToListAsync();
+            return View(viewModel);
+        }
+
+        // GEt: Klant gefilterd op naam en voornaam
+        public async Task<IActionResult> Search(ListKlantViewModel viewModel)
+        {
+            var result = _context.Klanten.AsQueryable();
+
+            if (!string.IsNullOrEmpty(viewModel.NaamSearch))
+            {
+                result = result.Where(k => k.Naam.StartsWith(viewModel.NaamSearch));
+            }
+            if (!string.IsNullOrEmpty(viewModel.VoornaamSearch))
+            {
+                result = result.Where(k => k.Voornaam.StartsWith(viewModel.VoornaamSearch));
+            }
+
+            viewModel.Klanten = await result.ToListAsync();
+
+            return View("Index", viewModel);
+        }
+
+        // GET: Klant/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var klant = await _context.Klanten
+                .FirstOrDefaultAsync(m => m.KlantID == id);
+            if (klant == null)
+            {
+                return NotFound();
+            }
+
+            return View(klant);
+        }
+
+        // GET: Klant/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Klant/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("KlantID,Naam,Voornaam,AangemaaktDatum")] Klant klant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(klant);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(klant);
+        }
+
+        // GET: Klant/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var klant = await _context.Klanten.FindAsync(id);
+            if (klant == null)
+            {
+                return NotFound();
+            }
+            return View(klant);
+        }
+
+        // POST: Klant/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("KlantID,Naam,Voornaam,AangemaaktDatum")] Klant klant)
+        {
+            if (id != klant.KlantID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(klant);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KlantExists(klant.KlantID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(klant);
+        }
+
+        // GET: Klant/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var klant = await _context.Klanten
+                .FirstOrDefaultAsync(m => m.KlantID == id);
+            if (klant == null)
+            {
+                return NotFound();
+            }
+
+            return View(klant);
+        }
+
+        // POST: Klant/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var klant = await _context.Klanten.FindAsync(id);
+            _context.Klanten.Remove(klant);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KlantExists(int id)
+        {
+            return _context.Klanten.Any(e => e.KlantID == id);
+        }
+    }
+}
---
+++ Data/HelloCoreContext.cs
@@ -0,0 +1,30 @@
+﻿using HelloCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace HelloCore.Data
+{
+    public class HelloCoreContext : IdentityDbContext<IdentityUser>
+    {
+        public HelloCoreContext (DbContextOptions<HelloCoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Klant> Klanten { get; set; }
+        public DbSet<Bestelling> Bestellingen { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Klant>().ToTable("Klant");
+            modelBuilder.Entity<Bestelling>().ToTable("Bestelling").Property(p => p.Prijs).HasColumnType("decimal(18,2)");
+        }
+    }
+}
